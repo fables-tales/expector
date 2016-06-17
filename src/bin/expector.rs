5c164ce1d94282e0c12ceefca7c99d9fa7fa008e
Sam Phippen <samphippen@googlemail.com>
2016-06-17 14:49:47 +0100

    With apologies to ag_dubs

---
+++ src/bin/expector.rs
@@ -0,0 +1,10 @@
+extern crate expector;
+use expector::*;
+
+fn main() {
+    expect("foo").to(eq(1));
+
+    let mut bees: Vec<f64> = Vec::new();
+    bees.push(37.0);
+    expect(bees).to(be_an_empty_vector());
+}
